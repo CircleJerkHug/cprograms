99fe4d616717bd001fa1857e72992dc27417cbd3
root <root@localhost.localdomain>
2018-10-19 07:53:02 +0530

    Modified the program to implementation to Longest commom subsequence through Dynamic Programming

--- longest_common_subseq_dp.c
+++ longest_common_subseq_dp.c
@@ -3,62 +3,70 @@
 #define maxn 100100
 int max(int a,int b)
 {
-        return a>b?a:b;
+        return (a>b)?a:b;
 }
-int LongestCommonSubsequence(char S[],char T[])
+void LongestCommonSubsequence(char S[],char T[],int m,int n)
 {
-        int Slength = strlen(S);
-        int Tlength = strlen(T);
-        /* Starting the index from 1 for our convinience (avoids handling special cases for negative indices) */
-        int iter,jter;
-        for(iter=Slength;iter>=1;iter--)
-        {
-                S[iter] = S[iter-1];
-        }
-        for(iter=Tlength;iter>=1;iter--)
-        {
-                T[iter] = T[iter-1];
-        }
-        int common[Slength+1][Tlength+1];
-        /* common[i][j] represents length of the longest common sequence in S[1..i], T[1..j] */
-        /* Recurrence:  common[i][j] = common[i-1][j-1] + 1 if S[i]==T[j]
-                                     = max(common[i-1][j],common[i][j-1]) otherwise
-        */      
-        /*common[0][i]=0, for all i because there are no characters from string S*/
-        for(iter=0;iter<=Tlength;iter++)
-        {
-                common[0][iter]=0;
-        }
-        /*common[i][0]=0, for all i because there are no characters from string T*/
-        for(iter=0;iter<=Slength;iter++)
-        {
-                common[iter][0]=0;
-        }
-        for(iter=1;iter<=Slength;iter++)
-        {
-                for(jter=1;jter<=Tlength;jter++)
-                {
-                        if(S[iter] == T[jter] )
-                        {
-                                common[iter][jter] = common[iter-1][jter-1] + 1;
-                        }
-                        else
-                        {
-                                common[iter][jter] = max(common[iter][jter-1],common[iter-1][jter]);
-                        }
-
-                }
-        }
-        return common[Slength][Tlength];
+    /* Starting the index from 1 for our convinience (avoids handling special cases for negative indices) */
+        int L[m+1][n+1]; 
+  
+   /* Following steps build L[m+1][n+1] in bottom up fashion. Note 
+      that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
+   for (int i=0; i<=m; i++) 
+   { 
+     for (int j=0; j<=n; j++) 
+     { 
+       if (i == 0 || j == 0) 
+         L[i][j] = 0; 
+       else if (S[i-1] == T[j-1]) 
+         L[i][j] = L[i-1][j-1] + 1; 
+       else
+         L[i][j] = max(L[i-1][j], L[i][j-1]); 
+     } 
+   } 
+        
+	   int index = L[m][n]; 
+	  
+	   // Create a character array to store the lcs string 
+	   char lcs[index+1]; 
+	   lcs[index] = ' '; // Set the terminating character 
+	  
+	   // Start from the right-most-bottom-most corner and 
+	   // one by one store characters in lcs[] 
+	    int i = m, j = n; 
+	   while (i > 0 && j > 0) 
+	   { 
+	      // If current character in X[] and Y are same, then 
+	      // current character is part of LCS 
+	      if (S[i-1] == T[j-1]) 
+	      { 
+	          lcs[index-1] = S[i-1]; // Put current character in result 
+	          i--; j--; index--;     // reduce values of i, j and index 
+	      } 
+	  
+	      // If not same, then find the larger of two and 
+	      // go in the direction of larger value 
+	      else if (L[i-1][j] > L[i][j-1]) 
+	         i--; 
+	      else
+	         j--; 
+   } 		
+  
+   // Print the lcs 
+   printf("%s\n",lcs);
+   printf("%d\n",L[m][n]);
 
 }
 int main()
 {
         char S[maxn],T[maxn];/* S,T are two strings for which we have to find the longest common sub sequence. */
+		int m,n;
         printf("Enter the first string:\n");
-        scanf("%s",T); 
+        scanf("%s",S); 
+        m=strlen(S);
         printf("Enter the second string:\n");
         scanf("%s",T);
-        printf("%d\n",LongestCommonSubsequence(S,T));
-
+        n=strlen(T);
+        LongestCommonSubsequence(S,T,m,n);
+        return 0;
 }
